4be045d92300860dcbf1ccc65bd2dbc0484af00e
tatendabakozw <tatendabakozw@gmail.com>
2024-07-29 22:51:24 +0200

    feat: added date picker optiin

--- src/components/sortable-item/SortableItem.tsx
+++ src/components/sortable-item/SortableItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { ArrowLongDownIcon, ArrowLongUpIcon } from "@heroicons/react/16/solid";
 
 export function SortableItem(props: any) {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -19,7 +20,11 @@ export function SortableItem(props: any) {
         style={{ touchAction: "none" }}
         className="cursor-move p-2 bg-white  border border-zinc-200/50 rounded-xl w-1/3 mx-auto"
       >
-        <div className="w-14 p-[2px] rounded-full  bg-zinc-200 mx-auto" />
+        <div className="flex w-full flex flex-row items-center">
+          <ArrowLongDownIcon height={12} width={12} />
+          <div className=" flex-1 h-1 rounded-full  bg-zinc-200 mx-auto" />
+          <ArrowLongUpIcon height={12} width={12} />
+        </div>
       </div>
       {props.children}
     </div>
