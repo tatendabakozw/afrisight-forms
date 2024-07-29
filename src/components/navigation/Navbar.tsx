86cfbeb5fb11579e1c3a181259b55de792918fa8
tatendabakozw <tatendabakozw@gmail.com>
2024-07-29 22:54:18 +0200

    fix: added dragable icon

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar({}: Props) {
   return (
     <div className="flex flex-col items-center py-4 border-b fixed z-50 top-0 w-full bg-white border-zinc-200/50">
       <div className="flex flex-row items-center max-w-7xl justify-between w-full mx-auto px-4">
-        <p>Afrisight Forms</p>
+        <p className="text-lg font-semibold text-zinc-950">Afrisight Forms</p>
         <div className="md:flex hidden space-x-4 flex-row items-center"></div>
         <div className="flex bg-brand-original text-white px-4 p-2 rounded-xl">
           Create Form
